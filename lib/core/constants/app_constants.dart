d8579935aa41891d5f3767cb8caf8cf324b831e2
engromjanali <engromjanali@gmail.com>
2026-05-03 22:36:20 +0600

    adde auth apis

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,43 +1,36 @@
 
 class AppConstants {
-  static const String appName = '6amMart';
+  static const String appName = 'e-sports';
   static const String appVersion = '1.0.0'; ///Flutter sdk 3.41.7
 
   static const String fontFamily = 'Roboto';
   static const bool payInWevView = false;
   static const int balanceInputLen = 10;
   static const String webHostedUrl = 'https://6ammart-test-web.6amdev.xyz';
-  // static const String webHostedUrl = 'https://6ammart-web.6amtech.com';
+
   static const bool useReactWebsite = false;
   static const bool stopPolylineAnimation = false;
   static const String googleServerClientId = '491987943015-agln6biv84krpnngdphj87jkko7r9lb8.apps.googleusercontent.com';
   static const String pusherBroadcustUrl = '/api/v1/broadcasting/user-auth';
 
-  // static const String baseUrl = 'https://6ammart-dev-testing.6amdev.xyz';
-  static const String baseUrl = 'https://6ammart-dev.6amdev.xyz';
+  static const String baseUrl = 'https://foatball.vercel.app';
 
-  /// end points
-  static const String reelListUri = '/api/v1/customer/reels/list';
+  /// auth endpoints
+  String configUri = "/api/user/config";
+  String registationUri = "/api/user/registation";
+  String loginUri = "/api/user/login";
+  String forgetPaasswordUri = "/api/user/forget-password";
+  String ProfileUri = "/api/user/profile";
 
 
   /// Shared Key
   static const String theme = '6ammart_theme';
 
   ///taxi
-  static const String taxiSearchHistory = '6ammart_taxi_search_history';
+
   static const String taxiSearchAddressHistory = '6ammart_taxi_search_address_history';
 
   static const String prescriptionMediaLibrary = 'prescription_media_library';
-
-  static const String topic = 'all_zone_customer';
-  static const String zoneId = 'zoneId';
-  static const String operationAreaId = 'operationAreaId';
-  static const String moduleId = 'moduleId';
-  static const String cacheModuleId = 'cacheModuleId';
-  static const String localizationKey = 'X-localization';
-  static const String latitude = 'latitude';
-  static const String longitude = 'longitude';
-  static const String cookiesManagement = 'cookies_management';
 
   // static List<ChooseUsModel> whyChooseUsList = [
   //   ChooseUsModel(icon: Images.landingTrusted, title: 'trusted_by_customers_and_store_owners'),
@@ -46,36 +39,6 @@ class AppConstants {
   //   ChooseUsModel(icon: Images.landingCheckout, title: 'easy_checkout_and_payment_system'),
   // ];
 
-  /// order status..
-  static const String pending = 'pending';
-  static const String accepted = 'accepted';
-  static const String processing = 'processing';
-  static const String confirmed = 'confirmed';
-  static const String handover = 'handover';
-  static const String pickedUp = 'picked_up';
-  static const String delivered = 'delivered';
-  static const String canceled = 'canceled';
-  static const String failed = 'failed';
-  static const String refunded = 'refunded';
-  static const String returned = 'returned';
-
-  /// Rider_module.
-  static const String ongoing = 'ongoing';
-  static const String completed = 'completed';
-  static const String cancelled = 'cancelled';
-
-  ///modules..
-  static const String pharmacy = 'pharmacy';
-  static const String food = 'food';
-  static const String parcel = 'parcel';
-  static const String ecommerce = 'ecommerce';
-  static const String grocery = 'grocery';
-  static const String taxi = 'rental';
-  static const String ride = 'ride-share';
-  static const String service = 'service';
-
-  ///ride share map zoom
-  static const double mapZoom = 20;
 
   // static List<LanguageModel> languages = [
   //   LanguageModel(imageUrl: Images.english, languageName: 'English', countryCode: 'US', languageCode: 'en'),
